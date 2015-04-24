9755ba6f6537fae97dee1739b8e527e25a562c95
hannahx <milger@kth.se>
2015-04-24 12:22:30 +0200

    nu? :)

--- Assets/Scripts/Behavior Tree Nodes/Actions/AvoidCollision.js
+++ Assets/Scripts/Behavior Tree Nodes/Actions/AvoidCollision.js
@@ -5,30 +5,25 @@ public class AvoidCollision extends ActionNode {
      
     var car : AICar_Script;
     private var closeCar : AICar_Script;
-    private var timer : int;
      
     // Called when the node starts its execution
     function Start () 
     {
-    	car.setBrakePower(300);
-    	timer = Time.time;
+    	car.setBrakePower(car.getBrakePower()+50);
     }
      
     // This function is called when the node is in execution
-    function Update () : Status {
-        // Do stuff
-  		if(Time.time > timer+0.5 && Time.time < timer+0.6 )
-        {
-        	car.setBrakePower(40);
-        	//return Status.Success;
-		}         
-        if(Time.time > timer+1 && car.getCloseCar()==false)
-        {
-        	//Debug.Log("drive");
-        	car.setBrakePower(0);
-        	//return Status.Success;
+    function Update () : Status 
+    {
+		if(car.getCloseCar()==false)
+		{
+			car.setBrakePower(0);
+			return Status.Success;
 		}
 		else
+		{
+			car.setBrakePower(car.getBrakePower()+20);
 			return Status.Running;
+		}
     }
 }
